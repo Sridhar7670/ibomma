9e996ebcf9d56b6360ff3ea9b7a38ef20921dde3
sridhar7670 <sridharnani080@gmail.com>
2025-06-07 13:55:17 +0530

    finding error

--- routes/routes.js
+++ routes/routes.js
@@ -7,11 +7,11 @@ manual_route.get("/", (req, res) => {
   res.send("Backend is live!");
 });
 
-manual_route.get("/search/:movieName",searchmovie)
-manual_route.get("/all",getall)
-manual_route.post("/register",register);
-manual_route.post("/login",login)
-manual_route.get('/favourites', authenticateToken, getFavourites);
-manual_route.post('/favourites', authenticateToken, addFavourite);
-manual_route.delete('/favourites/:movieName', authenticateToken, removeFavourite);
-module.exports = manual_route
+// manual_route.get("/search/:movieName",searchmovie)
+// manual_route.get("/all",getall)
+// manual_route.post("/register",register);
+// manual_route.post("/login",login)
+// manual_route.get('/favourites', authenticateToken, getFavourites);
+// manual_route.post('/favourites', authenticateToken, addFavourite);
+// manual_route.delete('/favourites/:movieName', authenticateToken, removeFavourite);
+// module.exports = manual_route
